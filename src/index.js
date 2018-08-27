25e3ba5138786e664cb9a9bd52ad40addd6a65bc
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2018-08-27 17:21:40 +0900

    fix comment

--- src/index.js
+++ src/index.js
@@ -16,9 +16,15 @@ var Routeful = function() {
   this._base = '/';
   this._root = '';
   this._stack = [];
+  // 履歴の数だけ、空の配列を生成する
   this._history = new Array(history.length);
+  // 現在のページに history.state が設定されていて、想定のフォーマットならそのまま history.state をセットする
+  // history.state が想定外のフォーマットの場合は、 history.length - 1 を設定する
+  // state は this._history のインデックス
   this.state = this.testStateFormat(history.state) ? history.state : history.length - 1;
+  // 現在のページのURLをキャッシュしておく
   this._history[this.state] = location.href.replace(location.origin, '');
+  // 現在のページに state (index) を設定しておく
   history.replaceState(this.state, null, this._history[this.state]);
 };
 
@@ -61,12 +67,14 @@ Routeful.prototype.go = function(path, replace) {
 
   if (replace === true) {
     history.replaceState(this.state, null, path);
+    // 今のURLを再キャッシュする
     this._history[this.state] = location.href.replace(location.origin, '');
   }
   else {
     history.pushState(this.state + 1, null, path);
     this._pushHistory();
   }
+  // 戻ったか進んだかのフラグを false にする
   this.isBack = false;
   this.isForward = false;
   // query は無視する
@@ -101,6 +109,9 @@ Routeful.prototype.emit = function(path) {
   return this;
 };
 
+/**
+ * 履歴のインデックスを進めて、URLをキャッシュする
+ */
 Routeful.prototype._pushHistory = function() {
   this.state++;
   if (this.state < this._history.length) {
@@ -110,26 +121,42 @@ Routeful.prototype._pushHistory = function() {
   return this;
 };
 
+/**
+ * routeful で想定する、 state のフォーマットかどうか
+ * @param {*} state 
+ */
 Routeful.prototype.testStateFormat = function(state) {
   return typeof state === 'number';
 };
 
 Routeful.prototype.popState = function(state) {
   this.isLegacy = !this.testStateFormat(state);
+  // 想定外のフォーマットの場合は、旧仕様のフラグを立てて、必要最低限の処理のみ実行する
   if (this.isLegacy) {
     return this;
   }
+  // 戻ったフラグ
   this.isBack = this.state > state;
+  // 進んだフラグ
   this.isForward = this.state < state;
+  // state (index) の更新
   this.state = state;
+  // URL をキャッシュ
   this._history[this.state] = location.href.replace(location.origin, '');
   return this;
 };
 
+/**
+ * 現在のページのパスを取得
+ */
 Routeful.prototype.getCurrent = function() {
   return this._current;
 };
 
+/**
+ * 前のページのパスを取得
+ * 取得できない場合は null
+ */
 Routeful.prototype.getPrev = function() {
   if (this.isLegacy) {
     return null;
@@ -141,6 +168,10 @@ Routeful.prototype.getPrev = function() {
   return url.replace(this._root, '').replace(this._base, '/');
 };
 
+/**
+ * 次のページのパスを取得
+ * 取得できない場合は null
+ */
 Routeful.prototype.getNext = function() {
   if (this.isLegacy) {
     return null;
@@ -152,6 +183,9 @@ Routeful.prototype.getNext = function() {
   return url.replace(this._root, '').replace(this._base, '/');
 };
 
+/**
+ * 戻れるかどうか
+ */
 Routeful.prototype.canBack = function() {
   if (this.isLegacy) {
     return false;
@@ -159,6 +193,9 @@ Routeful.prototype.canBack = function() {
   return this.state > 0;
 };
 
+/**
+ * 進めるかどうか
+ */
 Routeful.prototype.canForward = function() {
   if (this.isLegacy) {
     return false;
@@ -200,7 +237,7 @@ var onpopstate = function(e) {
     this.popState(e.state);
   }
   else if (e.type === 'hashchange') {
-    // location.hash = 'xxx' で変更した場合
+    // location.hash = 'xxx' で変更した場合は、 go で pushState した後と同じ状態になるように処理する
     if (!this.isLegacy && !this.testStateFormat(history.state)) {
       this._pushHistory();
       this.isBack = false;
